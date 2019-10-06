84371c3024dba9806f3a0c6bf5baaf33c64173a2
Grace Rapol <rapolgrace@gmail.com>
2019-10-06 00:04:10 -0400

    modified using javascript

--- app.js
+++ app.js
@@ -1,63 +1,4 @@
-
-
-$(document).ready(function() { 
-$("#calculate").click(function() {
-	var investment = ParseInt($("#investment").val());
-	var rate = ParseInt($("#annual_rate").val()); 
-	var years = ParseInt($("#years").val()); 
-	var isValid =true;
-			
-	if($(":investment").val()==""){
-		$("#investment").next().text("Please enter a value");
-	//$("#investment").next().text("No Value"); 
-	}
-	else if (isNaN($("#investment").val())){
-	$("#investment").next().text("Please enter a number");}
-else if(investment<100}} investment>100000)
-{
-$("#investment").next().text("Must be an integer from 100 to 100000");	
-}
-	else {
-			$("#investment").next().text("*");
-			}
-	if($(":annual_rate").val()==""){
-		$("#annual_rate").next().text("Please enter a value");
-	//$("#rate").next().text("No Value"); 
-	}
-	else if (isNaN($("#annual_rate").val())){
-	$("#annual_rate").next().text("Please enter a number");}
-	else if(<100}} investment>100000)
-{
-$("#annual_rate").next().text("Annual rate must be 1 to 10");	
-}
-else {
-		$("#annual_rate").next().text("*");
-			}
-	if($(":years").val()==""){
-		$("#years").next().text("Please enter a value");
-	//$("#rate").next().text("No Value"); 
-	}
-	else if (isNaN($("#years").val())){
-	$("#years").next().text("Please enter a number");}
-	else {
-			$("#years").next().text("*");
-			}
-					
-if( isValid ==true){
-	$("future_value").val = calculateFV(investment, rate, years);}
-})
-});
-var calculateFV = function(investment, rate, years) {
-var futureValue = investment;
-    for (var i = 1; i <= years; i++ ) {
-futureValue += futureValue * rate / 100;
-    }
-    futureValue = futureValue.toFixed(2);
-return futureValue;
-
-	}
-
-/*"use strict";
+"use strict";
 var $ = function (id) {
     return document.getElementById(id);
 }
@@ -97,8 +38,7 @@ $("years").nextElementSibling.firstChild.nodeValue= "Must be an integer from 1 t
 isValid=false;
 }
 else if (years < 1 || years > 25)
-{ 
-$("investment").nextElementSibling.firstChild.nodeValue= "Must be Integer from 100 to 100,000";
+{ $("investment").nextElementSibling.firstChild.nodeValue= "Must be Integer from 100 to 100,000";
 $("annual_rate").nextElementSibling.firstChild.nodeValue= "Must be a value from 1 to 12";
 $("years").nextElementSibling.firstChild.nodeValue= "Must be an integer from 1 to 50";
 isValid=false;
@@ -109,32 +49,15 @@ $("future_value").value = calculateFV(investment, rate, years);
 }
 }
 
-
-var button = document.getElementById("clear"),
-  count = 0;
-button.onclick = function() {
-  count += 1;
-	if(count==1){
-	var clearValues = function () {
-	$("investment").value= "";
-	$("annual_rate").value= "";
-	$("years").value= "";
-	$("future_value").value ="";
-}}
-  if(count==2)
-	 { 
-		 var clearValues = function () {
-	$("investment").nextElementSibling.firstChild.nodeValue= "";
-	$("annual_rate").nextElementSibling.firstChild.nodeValue= "";
-	$("years").nextElementSibling.firstChild.nodeValue= ""; 
-	
-} }
-	  
-};
-
+var clearValues = function () {
+$("investment").value= "";
+$("annual_rate").value= "";
+$("years").value= "";
+$("future_value").value ="";
+}
 
 window.onload = function () {
     $("calculate").onclick = processEntries;
 $("clear").onclick = clearValues;
     $("investment").focus();
-}*/
+}
